27b77e83381be4ca5f2fe7a1817e82713a25ac2b
Dana <dhagist@gmail.com>
2025-08-11 15:26:15 -1000

    Update test_fred.py

--- src/test_fred.py
+++ src/test_fred.py
@@ -1,7 +1,5 @@
-# src/test_fred.py
+# # src/test_fred.py
 from pandas_datareader import data as pdr
-
-codes = ["NAPM","USSLIND","T10Y3M"]
-for c in codes:
+for c in ["NAPM","USSLIND","T10Y3M"]:
     df = pdr.DataReader(c, "fred", start="1990-01-01")
     print(c, "rows:", len(df))
